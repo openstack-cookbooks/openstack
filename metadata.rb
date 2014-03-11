91d77ff55acb0b8fd322c91725bee6bcf996af4a
Choe, Cheng-Dae <whitekid@gmail.com>
2014-03-12 03:20:40 +0900

    database cookbook 의존성 추가

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name             'openstack'
+maintainer       'Choe, Cheng-Dae'
+maintainer_email 'whitekid@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures openstack'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.0'
+
+supports 'ubuntu', '= 12.04'
+
+depends "database"
+
